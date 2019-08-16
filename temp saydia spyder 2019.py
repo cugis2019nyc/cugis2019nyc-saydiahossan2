5e7ec01ef65f684dc95407fbc57fb1e5d9363e0c
saydiahossan2 <54040066+saydiahossan2@users.noreply.github.com>
2019-08-16 12:15:44 -0700

    Add files via upload

--- temp saydia spyder 2019.py
+++ temp saydia spyder 2019.py
@@ -53,4 +53,140 @@ def add(a,b):
 
 add(908,675)
 
-def add(a,c)
+
+
+def add(a,c):
+
+import math
+dir(math)
+
+
+math.sq
+
+
+
+
+
+import math
+dir (math)
+
+
+math.factorial.
+
+
+#dict data structure
+chocolate1 = {"milk":5}
+chocolate2 = {"dark" :6}
+chocolate3 = {"white" :8}
+
+
+
+milk=8
+milk= milk+46
+
+
+
+
+
+chocolatebox  = {"milk":5,"dark": 6,"white":8}
+chocolatebox
+
+
+#list
+student1 = ["Steve", 32, "M"]
+student2 = ["Lia", 28, "F"]
+student3 = ["Vin", 45, "M"]
+student4 = ["Katie", 38, "F"]
+
+
+student1[0]
+student1[1]
+student1[3]
+print(student1, student2, student3, student4)
+
+
+print[student1, student2, student3, student4]
+students
+studentinfo = [["Steve",32,"M"],["Lia",28,"F"],["Vin",45,"F"],["Katie",38,"F"]]
+
+
+
+#pandas
+
+import pandas
+dir(pandas)
+
+studentdf = pandas.DataFrame(studentinfo,columns=("Name","Age","Gender"))
+studentdf
+
+#reference columns names
+
+
+#read data file
+af = pandas.read_excel("GISdata.xlsx", sheet_name = women occupation)
+
+
+
+#chocolate milk dark white
+
+chocolate1 = ["Milk", 5]
+chocolate2 = ["Dark", 6]
+chocolate3 = ["White", 8]
+
+chocolate1 = [1]
+chocolate2[1]
+
+
+sugar=[chocolate1, chocolate2, chocolate3]
+sugar
+sugardf=pandas.DataFrame(sugar,columns=("type","quantity"))
+sugardf
+print [chocolate1, chocolate2, chocolate3]
+
+
+
+suagr=[chocolate1,chocolate2,chocolate3]
+sugar
+sugardf
+
+
+
+import plotly
+dir (plotly)
+from plotly. offline import plot
+import plotly.graph_objs as go
+
+studentbar = go.Bar(x=studentdf["Name"], y = studentdf["Age"])
+plot([studentbar])
+
+
+#DAY 4 
+import pandas
+studentlist = [["Seve", 32, "male"], ["Lia", 28, "female"], ["Vin", 45, "male"], ["Katie", 38, "female"]]
+print(studentlist)
+
+studentlistdf1 = pandas.Dataframe(studentlist, columns = ["name", "age", "gender"])
+
+
+
+agename = go.Bar(x=studentlistdf["name"], y=studentlistdf["age"])
+
+
+
+import pandas
+dir (pandas)
+import plotly
+from plotly. offline import plot
+import plotly.graph_objs as go
+
+
+wodf = pandas.read_excel("GISdata.xlsx", sheet_name = "womenOccupation")
+wodf
+
+
+
+woplot = go.Bar(x=wodf["occupation"], y=wodf["women"],
+                marker = {"color": wodf["women"],
+                            "colorscale" : "Jet"})
+plot([woplot])
+
